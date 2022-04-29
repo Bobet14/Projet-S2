f1a69e70d998ee41643c02d2eec2e46a0a27a181
Sgass <Sgass@SGASS-PC>
2022-04-29 10:28:32 +0200

    Noeuds

---
+++ Projet_S2/src/main/java/fr/insa/sgass/s2/Noeud.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.insa.sgass.s2;
+
+/**
+ *
+ * @author Sgass
+ */
+public abstract class Noeud {
+    
+}
