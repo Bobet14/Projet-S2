8ec9392376602993181caa80cb4215218172b1dc
Sgass <Sgass@SGASS-PC>
2022-04-29 10:46:17 +0200

    Noeuds

--- Projet_S2/src/main/java/fr/insa/sgass/s2/Noeud.java
+++ Projet_S2/src/main/java/fr/insa/sgass/s2/Noeud.java
@@ -10,4 +10,37 @@ package fr.insa.sgass.s2;
  */
 public abstract class Noeud {
     
+    private int ID;
+    private Vecteur pos;
+    private Vecteur Effort;
+    private int type;
+
+    public Noeud(int ID, Vecteur pos, Vecteur Effort, int type) {
+        this.ID = ID;
+        this.pos.setVecteur(pos);
+        this.Effort.setVecteur(Effort);
+        this.type=type;
+    }
+
+    public Noeud(Vecteur pos, Vecteur Effort, int type) {
+        this.pos.setVecteur(pos);
+        this.Effort.setVecteur(Effort);
+        this.ID=-1;
+        this.type=type;
+    }
+
+    public Noeud(Vecteur pos, int type) {
+        this.pos.setVecteur(pos);
+        this.Effort = new Vecteur (0,0);
+        this.ID=-1;
+        this.type=type;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
 }
